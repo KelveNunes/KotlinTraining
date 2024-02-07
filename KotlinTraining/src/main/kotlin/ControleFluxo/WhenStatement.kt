69232868a37106d9424c5ac0a2d1c85c64d5ddb0
kelve <kelvenunes123@gmail.com>
2024-02-07 13:54:47 -0300

    refatorando classes2

--- KotlinTraining/src/main/kotlin/ControleFluxo/WhenStatement.kt
+++ KotlinTraining/src/main/kotlin/ControleFluxo/WhenStatement.kt
@@ -2,19 +2,23 @@ package ControleFluxo
 
 import Functions.multiply
 
-class whenExpression {
-}
-fun whenAssing(numero: Int, numero2: Int) : Any{
-    var mutiply = multiply(numero,numero2)
-    var result = when(mutiply){
-        2 -> "dois"
-        4 -> "quatro"
-        6 -> "seis"
-        else -> "Nenhum dos resultados"
+class WhenStatement {
+    fun whenAssing(numero: Int, numero2: Int) : Any{
+
+        var mutiply = multiply(numero,numero2)
+        var result = when(mutiply){ //O when se comporta igual o switch case em java
+            2 -> "dois"
+            4 -> "quatro"
+            6 -> "seis"
+            else -> "Nenhum dos resultados"
+        }
+        return result
     }
-    return result
-}
-fun main(){
-    println(whenAssing(2,1))
 }
 
+
+
+fun main(){
+    println(WhenStatement().whenAssing(2,1))
+
+}
