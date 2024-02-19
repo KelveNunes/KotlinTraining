c83596952ac2466be7ff526b42e4aa7b8d91f4a1
kelve <kelvenunes123@gmail.com>
2024-02-19 11:29:10 -0300

    refatorando o codigo

--- KotlinTraining/src/main/kotlin/MainKotlin/Main.kt
+++ KotlinTraining/src/main/kotlin/MainKotlin/Main.kt
@@ -33,4 +33,39 @@ fun main() {
     printMessageWithPrefix(prefix = "Log", message = "Hello") // 8
     println(sum(1, 2)) // 9
     println(multiply(2, 4)) // 10
+
+    val numeroRomano: String? = readLine()
+
+    val numerosRomanos = mapOf(
+            'I' to 1,
+            'V' to 5,
+            'X' to 10,
+            'L' to 50,
+            'C' to 100,
+            'D' to 500,
+            'M' to 1000
+    )
+
+    var resultado = 0
+
+    for (i in numeroRomano!!.indices) {
+        // Recupera o valor em romano do indice atual.
+        val atual = numerosRomanos.getValue(numeroRomano[i])
+        // Recupera o valor em romano do próximo indice (caso exista).
+        val proximo = when (i + 1) {
+            // Caso o próximo indice não exista, atribui 0 à variável $proximo.
+            numeroRomano.length -> 0
+            // Caso contrário, atribui o valor em romano equivalente ao próximo indice.
+            else -> numerosRomanos.getValue(numeroRomano[i + 1])
+        }
+
+        if (proximo<=atual) {
+            resultado += atual
+        }
+        else{
+            resultado -= atual
+        }
+    }
+
+    print(resultado)
 }
