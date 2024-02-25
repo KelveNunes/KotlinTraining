8ecbd9feec11d80437f05a9ea3cc45f9a9490d17
kelve <kelvenunes123@gmail.com>
2024-02-25 15:29:04 -0300

    criando customer controler get e delete

--- credit.application.system/credit.application.system/src/main/kotlin/me/dio/credit/application/system/controller/CustomerController.kt
+++ credit.application.system/credit.application.system/src/main/kotlin/me/dio/credit/application/system/controller/CustomerController.kt
@@ -2,8 +2,11 @@ package me.dio.credit.application.system.controller
 
 import me.dio.credit.application.system.controller.dto.CustomerWiew
 import me.dio.credit.application.system.dto.CustomerDto
+import me.dio.credit.application.system.entity.Customer
 import me.dio.credit.application.system.service.implemetation.CustomerService
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -19,8 +22,11 @@ class CustomerController(
         val savedCustomer = this.customerService.save(customerDto.toEntity())
         return "Customer ${savedCustomer.email} saved!"
     }
-    @GetMapping
-    fun findById(id: Long): CustomerWiew {
-
+    @GetMapping("/{id}")
+    fun findById(@PathVariable id: Long): CustomerWiew {
+       val customer: Customer = this.customerService.findById(id)
+        return CustomerWiew(customer)
     }
+    @DeleteMapping("/{id}")
+    fun deleteCustomer(@PathVariable id: Long) = this.customerService.delete(id)
 }
--- credit.application.system/credit.application.system/src/main/kotlin/me/dio/credit/application/system/controller/dto/CustomerWiew.kt
+++ credit.application.system/credit.application.system/src/main/kotlin/me/dio/credit/application/system/controller/dto/CustomerWiew.kt
@@ -1,5 +1,24 @@
 package me.dio.credit.application.system.controller.dto
 
-class CustomerWiew {
+import me.dio.credit.application.system.entity.Customer
+import java.math.BigDecimal
 
+data class CustomerWiew(
+    val firstName: String,
+    val lastName: String,
+    val cpf: String,
+    val income: BigDecimal,
+    val email: String,
+    val zipCode: String,
+    val street: String
+) {
+    constructor(customer: Customer): this(
+        firstName = customer.firstName,
+        lastName = customer.lastName,
+        cpf = customer.cpf,
+        income = customer.income,
+        email = customer.email,
+        zipCode = customer.address.zipCode,
+        street = customer.address.street
+    )
 }
